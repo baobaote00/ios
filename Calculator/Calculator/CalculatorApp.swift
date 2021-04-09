46083c520258e56ce4ba86020bcd5be2de7c0148
お金 <57231517+baobaote00@users.noreply.github.com>
2021-04-09 16:19:54 +0700

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Nguyen Tien on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
